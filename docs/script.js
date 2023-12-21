d89343fac8940845607d7efc860ff0b7f69a1f4d
tdwth-joe <153840684+tdwth-joe@users.noreply.github.com>
2023-12-21 16:55:07 -0600

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var showArticle = function() {
+        document.querySelector('.article-content').style.display = 'block';
+    };
 
+    var closeArticle = function() {
+        document.querySelector('.article-content').style.display = 'none';
+    };
+
+    var readButtons = document.querySelectorAll('.read-article-btn');
+    for (var i = 0; i < readButtons.length; i++) {
+        readButtons[i].addEventListener('click', showArticle);
+    }
+
+    var closeButton = document.querySelector('.account');
+    closeButton.addEventListener('click', closeArticle);
+});
